622642c995925b6446fc0a0aff3ca6e256a17dab
hoangnhuet <ngoh52170@gmail.com>
2025-03-14 17:19:24 +0700

    need check mpc.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from mpc import RobotMPC
 robot = Robot(X=[0, 0, 0, 0, 0, 0])
 mpc_controller = RobotMPC(robot)
 
-reference_state = [10, 0, 0, 0, 0, 0] 
+reference_state = [0.5, 0, 0, 0, 0, 0] 
 
 for i in range(1000):
     optimal_control = mpc_controller.solve(reference_state)
     left_torque, right_torque = optimal_control
-    robot.updateConfiguration(0.1,0.1, 0.02)
+    robot.updateConfiguration(left_torque,right_torque, 0.02)
     print(f"Time1: {i * 0.02}, Position: {robot.getPosition()}, Control: {optimal_control}")
     time.sleep(0.02)
--- mpc.py
+++ mpc.py
@@ -2,7 +2,7 @@ import casadi as ca
 import numpy as np
 import math
 class RobotMPC:
-    def __init__(self, robot, T=0.02, N=30, Q=np.diag([10.0, 0.0, 50.0, 0.0, 10.0, 0.0]), R=np.diag([1.0, 1.0])):
+    def __init__(self, robot, T=0.02, N=30, Q=np.diag([4.0, 0.0, 50.0, 0.0, 1.5, 0.0]), R=np.diag([1.0, 1.0])):
         self.robot = robot
         self.T = T  # Time step
         self.N = N  # Horizon length
@@ -25,9 +25,10 @@ class RobotMPC:
     def setup_controller(self):
         self.opti = ca.Opti()
         self.opt_controls = self.opti.variable(self.N, 2)  # (N x 2) control inputs (torques)
-        self.opt_states = self.opti.variable(self.N + 1, 6)  # (N+1 x 6) states
+        self.state0 = self.opti.parameter(6, 1)  # Initial state
+        self.opt_states = self.opti.variable(self.N , 6)  # (N+1 x 6) states
         self.reference_state = self.opti.parameter(6, 1)  # Column vector parameter for reference state
-        for i in range(self.N):
+        for i in range(self.N-1):
             # Explicitly unpack state variables
             x_ = self.opt_states[i, 0]
             dx_ = self.opt_states[i, 1]
@@ -37,23 +38,23 @@ class RobotMPC:
             dpsi_ = self.opt_states[i, 5]
             u_ = self.opt_controls[i, :]
             next_state = self.opt_states[i, :] + self.dynamics(x_, dx_, theta_, dtheta_, psi_, dpsi_, u_).T * self.T
+            # print(i,"OP: ",self.opt_states[i + 1, :].shape,"NS: ", next_state.shape)
             self.opti.subject_to(self.opt_states[i + 1, :] == next_state)
-        
         obj = 0
         for i in range(self.N):
             state_error = self.opt_states[i, :] - self.reference_state.T
             control_effort = self.opt_controls[i, :]
             state_cost = ca.mtimes([state_error, self.Q, state_error.T])
             control_cost = ca.mtimes([control_effort, self.R, control_effort.T])
-            
             obj += state_cost + control_cost
         
         self.opti.minimize(obj)
         
-        self.opti.subject_to(self.opti.bounded(-np.pi / 4, self.opt_states[:, 2], np.pi / 4))  # theta constraints
-        # self.opti.subject_to(self.opti.bounded(-1, self.opt_states[:, 3], 1))  # dtheta constraints
+        self.opti.subject_to(self.opti.bounded(-np.pi / 5, self.opt_states[:, 2], np.pi / 5))  # theta constraints
         self.opti.subject_to(self.opti.bounded(-1.5, self.opt_controls[:, 0], 1.5))  # right torque
         self.opti.subject_to(self.opti.bounded(-1.5, self.opt_controls[:, 1], 1.5))  # left torque
+        self.opti.subject_to(self.opti.bounded(-0.5, self.opt_states[:, 1], 0.5))  # dx constraints
+        self.opti.subject_to(self.opti.bounded(-1, self.opt_states[:, 3], 1)) 
         
         opts_setting = {
             'ipopt.max_iter': 2000,
@@ -64,11 +65,12 @@ class RobotMPC:
         }
         self.opti.solver('ipopt', opts_setting)
         self.opti.subject_to(self.opt_states[0, :].T == self.robot.X)
-    def solve(self, reference_state):      
+    def solve(self, reference_state):
+        # self.opti.subject_to(self.opt_states[0, :] == np.array([self.robot.X]))  
+        self.opti.set_value(self.state0, self.robot.X)
         self.opti.set_value(self.reference_state, np.array(reference_state).reshape(6, 1))
-        self.opti.set_initial(self.opt_states, self.next_states)
+        # self.opti.set_initial(self.opt_states, self.next_states)
         self.opti.set_initial(self.opt_controls, self.u0)
-        
         try:
             sol = self.opti.solve()
             u_res = sol.value(self.opt_controls)
